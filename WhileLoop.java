758f236f23b356fc614f3891287cbd5d1fee4e44
DibrajSarkar <hi.dibraj@gmail.com>
2021-04-20 02:44:48 +0600

    While Loop

---
+++ WhileLoop.java
@@ -0,0 +1,19 @@
+
+
+package javaBeginning;
+
+
+public class WhileLoop {
+    public static void main(String [] args ){
+    int i=1;
+    while(i<=10){
+        System.out.println(i + "Bangladesh");
+        i++;
+    }
+    int x=2;
+    while(x<=100){
+        System.out.println(x);
+        x+=2;
+    }
+    }
+}

053f93968ae0aad560e9a83b97d41bfc9c88f7fc
DibrajSarkar <hi.dibraj@gmail.com>
2021-04-20 02:35:30 +0600

    For Loop

---
+++ ForLoop.java
@@ -0,0 +1,19 @@
+
+package javaBeginning;
+
+public class ForLoop {
+    public static void main(String [] args ){
+    int x;
+    for(x=1;x<=10;x++){
+        System.out.println("Number:"+x);
+    }
+    int i;
+    for(i=0;i<=100;i++){
+            System.out.println(i);
+    }
+    int y;
+    for(y=2;y<=100;y+=2){
+    System.out.println(y);
+    }
+    }
+}

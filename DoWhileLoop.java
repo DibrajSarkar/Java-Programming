5e5d3ed0c308f7b07694ed48e477f6eb3834d509
DibrajSarkar <hi.dibraj@gmail.com>
2021-04-20 03:10:55 +0600

    DoWhile Loop

---
+++ DoWhileLoop.java
@@ -0,0 +1,13 @@
+
+package javaBeginning;
+
+
+public class DoWhileLoop {
+    public static void main(String [] args ){
+    int i=1;
+    do{
+            System.out.println("Bangladesh");
+            i++;
+         }while (i>=100);// When conditions false , but one time it's working. Do while first Do one print
+    }
+}

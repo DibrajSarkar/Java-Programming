6eac267b56adb60b2169f62eaadb337ce2ba8dfa
DibrajSarkar <hi.dibraj@gmail.com>
2021-04-19 15:45:28 +0600

    Bitwise Operator

---
+++ BitwiseOpertor.java
@@ -0,0 +1,22 @@
+
+package javaBeginning;
+
+
+public class BitwiseOpertor {
+    public static void main (String [] args){
+    int x=32;
+    int y=12;
+    int c;
+    c= x&y;
+    System.out.println("x & y = "+c);
+    c= x|y;
+    System.out.println("x | y = "+c);
+    c= x^y;
+    System.out.println("x ^ y = "+c);
+    c= x>>2;
+    System.out.println("x & y = "+c);
+    c= x<<2;
+    System.out.println("x & y = "+c);
+    
+    }
+}

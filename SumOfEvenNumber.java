75356cfc16112c150d41d0664e14717a7dc1387b
DibrajSarkar <hi.dibraj@gmail.com>
2021-04-20 16:25:51 +0600

    Sum of Even Number

--- SumOfEvenNumber.java
+++ SumOfEvenNumber.java
@@ -5,11 +5,18 @@ import java.util.Scanner;
 
 public class SumOfEvenNumber {
     public static void main(String[] args) {
-        int n,m,sum;
+        int n,m;
+        int sum=0;
         Scanner input=new Scanner(System.in);
         System.out.println("Enter Inital Number:");
         n=input.nextInt();
         System.out.println("Enter Final Number: ");
         m=input.nextInt();
+        for (int i = n; i <=m; i++) {
+            if(i%2==0){
+                sum+=i;
+            }
+        }
+        System.out.println("Sum of Even Number: "+sum);
     }
 }

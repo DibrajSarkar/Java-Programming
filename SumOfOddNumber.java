e85697b66fe20e32d1043463daba6a04c15ffb50
DibrajSarkar <hi.dibraj@gmail.com>
2021-04-20 17:12:34 +0600

    Sum of odd Number which user given

--- SumOfOddNumber.java
+++ SumOfOddNumber.java
@@ -3,22 +3,25 @@ package javaBeginning;
 
 import java.util.Scanner;
 
-public class SumOfEvenNumber {
+
+public class SumOfOddNumber {
     public static void main(String[] args) {
-        int n,m;
+        int i,n,m;
         int sum=0;
         Scanner input=new Scanner(System.in);
-        System.out.println("Enter Inital Number:");
+        System.out.println("Enter inital Number: ");
         n=input.nextInt();
         System.out.println("Enter Final Number: ");
         m=input.nextInt();
-        for (int i = n; i <=m; i++) {
-            if(i%2==0){
+        for ( i = n; i <=m; i++) {
+            if(i%2!=0){// i%2==1
+                System.out.println(" "+i);
                 sum+=i;
-                System.out.print("  "+i);
             }
         }
         System.out.println("");
-        System.out.println(" Sum of Even Number: "+sum);
-    }
+        System.out.println("Sum of All Odd Number: "+sum);
 }
+    
+}
+

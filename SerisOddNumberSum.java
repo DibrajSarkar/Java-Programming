c1869993024a7da528438b7d9db1d0ce7058254e
DibrajSarkar <hi.dibraj@gmail.com>
2021-04-21 16:15:31 +0600

    Sum of odd Number until the provide number

---
+++ SerisOddNumberSum.java
@@ -0,0 +1,20 @@
+
+package javaBeginning;
+
+import java.util.Scanner;
+
+
+public class SerisOddNumberSum {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int n,sum=0;
+        System.out.println("Enter last Number: ");
+        n=input.nextInt();
+        for (int i = 1; i < 10; i+=2) {
+            System.out.println(i+" ");
+            sum+=i;
+        }
+        System.out.println("");
+        System.out.println("Sum of Odd Number until your provide number: "+sum);
+    }
+}

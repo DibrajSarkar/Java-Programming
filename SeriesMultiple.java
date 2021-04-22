76c63c3183c58f90ded385665cdb9450fedc5379
DibrajSarkar <hi.dibraj@gmail.com>
2021-04-22 23:47:04 +0600

    Series Problem Multiple

---
+++ SeriesMultiple.java
@@ -0,0 +1,17 @@
+
+package javaBeginning;
+import java.util. Scanner;
+public class SeriesMultiple {
+    public static void main(String[] args) {
+        Scanner input=new Scanner (System.in);
+       int n,m;
+       int sum=0;
+        System.out.println("Enter inital number: ");
+        n=input.nextInt();
+        
+        for (int i = ;n i<=m; i++) {
+            System.out.println(i+" ");
+            
+        }
+    }
+}

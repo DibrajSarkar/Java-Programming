8c96e48c36bf53de673f4f2c9c35a08a62c5da5e
DibrajSarkar <hi.dibraj@gmail.com>
2021-04-24 00:41:10 +0600

    Series Problem

---
+++ SeriesProblem.java
@@ -0,0 +1,13 @@
+
+package javaBeginning;
+
+import java.util.Scanner;
+
+public class SeriesProblem {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        double num,result=1;
+        System.out.println("Enter last number: ");
+        num=input.nextDouble();
+    }
+}

db655c0d0012547581a5f19d22a1d6f2e14f8383
DibrajSarkar <hi.dibraj@gmail.com>
2021-04-24 16:57:24 +0600

    Prime Number

---
+++ PrimeNumberInput.java
@@ -0,0 +1,19 @@
+
+package javaBeginning;
+
+import java.util.Scanner;
+
+
+public class PrimeNumberInput {
+    public static void main(String[] args) {
+        int m,n;
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter Initial Number: ");
+        m=input.nextInt();
+        System.out.println("Enter Final Number: ");
+        n=input.nextInt();
+        for (int i = 0; i < 10; i++) {
+            
+        }
+    }
+}

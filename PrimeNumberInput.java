5f87b7340db28d0b6fd918cf0c4528e75c353a96
DibrajSarkar <hi.dibraj@gmail.com>
2021-04-24 17:12:50 +0600

    Prime Number m to n

--- PrimeNumberInput.java
+++ PrimeNumberInput.java
@@ -6,14 +6,30 @@ import java.util.Scanner;
 
 public class PrimeNumberInput {
     public static void main(String[] args) {
-        int m,n;
+        int m,n,count=0,TotalPrime=0;
         Scanner input=new Scanner(System.in);
         System.out.println("Enter Initial Number: ");
         m=input.nextInt();
+        
         System.out.println("Enter Final Number: ");
         n=input.nextInt();
-        for (int i = 0; i < 10; i++) {
+        
+        for (int i = m; i <=n; i++) {
+            
+            for (int j = 2; j <i-1; j++) {
+                if(i%j==0){
+                count++;
+                break;
+                }
+            }
+            if (count==0) {
+                System.out.println(i);
+                TotalPrime++;
+            }
+                count=0;
             
         }
+        System.out.println("TotalPrime: "+TotalPrime);
+    
     }
 }

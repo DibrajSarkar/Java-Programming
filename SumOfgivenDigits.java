bccc0a024bce8d05101049e2101304fe45f67d91
DibrajSarkar <hi.dibraj@gmail.com>
2021-04-26 23:42:53 +0600

    Sum Of Digits

---
+++ SumOfgivenDigits.java
@@ -0,0 +1,15 @@
+
+package javaBeginning;
+
+import java.util.Scanner;
+
+
+public class SumOfgivenDigits {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int sum=0,temp,num;
+        System.out.println("Enter Any Number: ");
+        num=input.nextInt();
+        
+    }
+}

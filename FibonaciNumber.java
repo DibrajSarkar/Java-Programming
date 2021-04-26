3fbeb401fb1b2f0a2fdbc7daf0ef99af6b708933
DibrajSarkar <hi.dibraj@gmail.com>
2021-04-26 19:18:10 +0600

    Fibonaci Number

---
+++ FibonaciNumber.java
@@ -0,0 +1,12 @@
+
+package javaBeginning;
+
+import java.util.Scanner;
+
+public class FibonaciNumber {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("How many Numbers: ");
+        int n=input.nextInt();
+    }
+}

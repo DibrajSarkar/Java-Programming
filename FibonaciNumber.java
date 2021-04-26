235aadd8eea434898e2553204481bdccd21b8bb5
DibrajSarkar <hi.dibraj@gmail.com>
2021-04-26 19:34:16 +0600

    Fibonaci Number

--- FibonaciNumber.java
+++ FibonaciNumber.java
@@ -8,5 +8,19 @@ public class FibonaciNumber {
         Scanner input=new Scanner(System.in);
         System.out.println("How many Numbers: ");
         int n=input.nextInt();
+        int first=0;
+        int second=1;
+        int fibo;
+        
+        System.out.print(first+" "+second);
+        
+        for (int i = 3; i <=n; i++) {
+            
+            fibo=first+second;
+            System.out.print( " "+fibo);
+            first=second;
+            second=fibo;
+        }
+        System.out.println(" ");
     }
 }

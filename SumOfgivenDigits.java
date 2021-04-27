4ad187325c7ee7a5ab4a14dfe600ecf4920d6b56
DibrajSarkar <hi.dibraj@gmail.com>
2021-04-27 23:56:59 +0600

    Sum of Digit

--- SumOfgivenDigits.java
+++ SumOfgivenDigits.java
@@ -7,9 +7,14 @@ import java.util.Scanner;
 public class SumOfgivenDigits {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
-        int sum=0,temp,num;
+        int sum=0,temp,num,r;
         System.out.println("Enter Any Number: ");
         num=input.nextInt();
+        temp=num;
         
+        while(temp!=0)
+        {
+        
+        }
     }
 }

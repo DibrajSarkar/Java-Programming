fa956ec1172b9e03012bac60082391ea446e425c
DibrajSarkar <hi.dibraj@gmail.com>
2021-05-01 01:43:10 +0600

    Reverse Digits

--- ReverseDigit.java
+++ ReverseDigit.java
@@ -7,14 +7,14 @@ import java.util.Scanner;
 public class ReverseDigit {
     public static void main(String[] args) {
         Scanner input =new Scanner (System.in);
-        int r,num,temp,sum=0;
+        int reminder,num,temp,sum=0;
         System.out.println("Enter Desire Number: ");
         num=input.nextInt();
         temp=num;
         while(temp!=0)
         {
-        r=temp%10;
-        sum=sum*10 +r;
+        reminder=temp%10;
+        sum=sum*10 +reminder;
         temp=temp/10;
         }
         System.out.println("Reverse Digit: "+sum);

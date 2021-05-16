cb970b3a28129d447da8663fb19e9616dc286fb5
DibrajSarkar <hi.dibraj@gmail.com>
2021-05-16 21:12:48 +0600

    Checking Number Palindrome or not

---
+++ PalindromeNumber.java
@@ -0,0 +1,20 @@
+
+package javaBeginning;
+
+import java.util.Scanner;
+
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int remainder,num,temp,sum=0;
+        System.out.println("Enter Your desire Number: ");
+        num=input.nextInt();
+        temp=num;
+        while(temp!=0)
+        {
+           remainder=temp%10;
+           sum=sum*10+remainder;
+        }
+    }
+}

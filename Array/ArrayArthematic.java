7281a71a792fe2f44149b940401bee79bce5d033
DibrajSarkar <hi.dibraj@gmail.com>
2021-05-24 00:18:27 +0600

    Doing Arithmatic Programme in an Array

---
+++ Array/ArrayArthematic.java
@@ -0,0 +1,17 @@
+
+package javaBeginning.Array;
+
+import java.util.Scanner;
+
+public class ArrayArthematic {
+    public static void main(String[] args) {
+        Scanner input=new Scanner (System.in);
+        double [] number =new double[5];
+        System.out.println("Enter Five Number: ");
+        number[0]=input.nextDouble();
+        number[1]=input.nextDouble();
+        number[2]=input.nextDouble();
+        number[3]=input.nextDouble();
+        number[4]=input.nextDouble();
+    }
+}

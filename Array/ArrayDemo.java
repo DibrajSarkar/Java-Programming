1dbd81d1655fb27767beb287e0aab2dcb2cc8a28
DibrajSarkar <hi.dibraj@gmail.com>
2021-05-24 00:04:49 +0600

    Array Basic

--- Array/ArrayDemo.java
+++ Array/ArrayDemo.java
@@ -1,9 +1,26 @@
-
 package javaBeginning.Array;
 
 public class ArrayDemo {
+
     public static void main(String[] args) {
-        int [] number=new int[5];
-        number[0]=12;
+        int[] number = new int[5];
+        number[0] = 10;
+        number[1] = 11;
+        number[2] = 12;
+        number[3] = 14;
+        number[4] = 15;
+        int len=number.length;
+        
+        System.out.println(len);
+        System.out.println(number[3]);
+        
+        double [] num=new double[3];
+        num[0]=1.11;
+        num[1]=1.12;
+        num[2]=1.13;
+        
+        System.out.println(num[2]);
+        
+
     }
 }

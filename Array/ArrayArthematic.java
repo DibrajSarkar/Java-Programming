d88fd75e8e5b6f09cdc12c963975850f139d72ef
DibrajSarkar <hi.dibraj@gmail.com>
2021-05-24 12:50:39 +0600

    Using loop in an Array

--- Array/ArrayArthematic.java
+++ Array/ArrayArthematic.java
@@ -24,7 +24,7 @@ public class ArrayArthematic {
         //sum=number[0]+number[1]+number[2]+number[3]+number[4];
         // Same Work Using Loop 
         for (int i = 0; i < 5; i++) {
-            sum=sum+number[i];
+            sum=sum+number[i];//0+1=1,1+2=3,3+3=6,6+4=10,10+5=15;
         }
         System.out.println("Sum of Five Number: "+sum);
     }

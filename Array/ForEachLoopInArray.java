b78d20147734d4b7c53e7281ad1cf1d81795e39d
DibrajSarkar <hi.dibraj@gmail.com>
2021-05-24 23:20:43 +0600

    For Each Loop in an Array

--- Array/ForEachLoopInArray.java
+++ Array/ForEachLoopInArray.java
@@ -8,8 +8,18 @@ public class ForEachLoopInArray {
         names[1]="Rion Al Noman";
         names[2]="Java";
         names[3]="QA Engineer";
-        for (int i = 0; i <names.length; i++) {
-            System.out.println(names[i]);
-        }
+        
+//        for (int i = 0; i <names.length; i++) {
+//            System.out.println(names[i]);
+//        }
+      for(String x : names){
+          System.out.println(x);
+      }
+      int [] num={10,12,13,15,14,100,200,144};
+      int sum=0;
+      for(int y : num){
+          sum=sum+y;
+      }
+      System.out.println("Total Sum is: "+sum);
     }
 }

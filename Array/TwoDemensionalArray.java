7154fefc958ffed842494eb47113bd62595df423
DibrajSarkar <hi.dibraj@gmail.com>
2021-05-25 00:03:36 +0600

    Start Learning two Demensional Array in java

---
+++ Array/TwoDemensionalArray.java
@@ -0,0 +1,11 @@
+
+package javaBeginning.Array;
+
+
+public class TwoDemensionalArray {
+    public static void main(String[] args) {
+        int [][]num=new int[2][3];
+        num[0][0]=12;
+        System.out.println(num[0][0]);
+    }
+}

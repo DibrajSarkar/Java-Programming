0615da8e02f7f15f587123bff1baf85108205a22
DibrajSarkar <hi.dibraj@gmail.com>
2021-05-25 00:07:19 +0600

    Two Dimensional Array

--- Array/TwoDemensionalArray.java
+++ Array/TwoDemensionalArray.java
@@ -6,6 +6,11 @@ public class TwoDemensionalArray {
     public static void main(String[] args) {
         int [][]num=new int[2][3];
         num[0][0]=12;
-        System.out.println(num[0][0]);
+        num[0][1]=12;
+        num[0][2]=22;
+        num[1][0]=12;
+        num[1][1]=12;
+        num[1][2]=12;
+        System.out.println(num[0][2]);
     }
 }

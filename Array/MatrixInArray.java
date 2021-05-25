1b724f83dad67806337c0fb6c47d2b25a16cccf4
DibrajSarkar <hi.dibraj@gmail.com>
2021-05-25 13:23:28 +0600

    Matrix in an Array

--- Array/MatrixInArray.java
+++ Array/MatrixInArray.java
@@ -11,9 +11,18 @@ public class MatrixInArray {
         int[][] A = new int[2][3];
         int[][] B = new int[2][3];
         
+        System.out.println("Enter Six Value: ");
+        // Getting input for A Matrix
         for (int row = 0; row <2; row++) {
             for (int col = 0; col < 3; col++) {
                 A[row][col]=input.nextInt();
+            }
+        }
+        //Printing A Matrix 
+        System.out.print("A : ");
+        for (int row = 0; row < 2; row++) {
+            for (int col = 0; col < 3; col++) {
+                System.out.print(" "+A[row][col]);
             }
         }
     }
--- Array/TwoDArray.java
+++ Array/TwoDArray.java
@@ -8,10 +8,15 @@ public class TwoDArray {
         Scanner input=new Scanner(System.in);
         int [] [] num=new int[2][3];
         System.out.println("Enter Six Value: ");
-        for (int rowfirst = 0; rowfirst < 2; rowfirst++) {
-            
-            for (int colfirst = 0; colfirst < 3; colfirst++) {
-               
+        for (int row = 0; row < 2; row++) {
+            for (int col = 0; col < 3; col++) {
+                num[row][col]=input.nextInt();
+            }
+        }
+        // Printing avobe value
+        for (int row = 0; row < 2; row++) {
+            for (int col = 0; col < 3; col++) {
+                System.out.print(" "+num[row][col]);
             }
         }
     }

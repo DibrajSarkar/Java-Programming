32c0b178d9a10981e579d897eebfb02a45592cce
DibrajSarkar <hi.dibraj@gmail.com>
2021-05-25 12:20:32 +0600

    Matrix in an Array

---
+++ Array/MatrixInArray.java
@@ -0,0 +1,20 @@
+package javaBeginning.Array;
+
+import java.util.Scanner;
+
+public class MatrixInArray {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int[][] A = new int[2][3];
+        int[][] B = new int[2][3];
+        
+        for (int row = 0; row <2; row++) {
+            for (int col = 0; col < 3; col++) {
+                A[row][col]=input.nextInt();
+            }
+        }
+    }
+}
---
+++ Array/TwoDArray.java
@@ -0,0 +1,18 @@
+
+package javaBeginning.Array;
+
+import java.util.Scanner;
+
+public class TwoDArray {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int [] [] num=new int[2][3];
+        System.out.println("Enter Six Value: ");
+        for (int rowfirst = 0; rowfirst < 2; rowfirst++) {
+            
+            for (int colfirst = 0; colfirst < 3; colfirst++) {
+               
+            }
+        }
+    }
+}

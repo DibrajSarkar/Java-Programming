5bf6cc7aa1432c8ccf966c4d483f34f8e60425cf
DibrajSarkar <hi.dibraj@gmail.com>
2021-05-25 13:52:48 +0600

    Input and print a matrix in Array

--- Array/MatrixInArray.java
+++ Array/MatrixInArray.java
@@ -10,20 +10,40 @@ public class MatrixInArray {
 
         int[][] A = new int[2][3];
         int[][] B = new int[2][3];
-        
-        System.out.println("Enter Six Value: ");
+
+        System.out.println("Enter six of A Matrix: ");
         // Getting input for A Matrix
-        for (int row = 0; row <2; row++) {
+        for (int row = 0; row < 2; row++) {
             for (int col = 0; col < 3; col++) {
-                A[row][col]=input.nextInt();
+                System.out.printf("A[%d] [%d] =", row, col);
+                A[row][col] = input.nextInt();
+            }
+        }
+        // Getting input for B Matrix
+        System.out.println("Enter six value for B Matrix: ");
+
+        for (int row = 0; row < 2; row++) {
+            for (int col = 0; col < 3; col++) {
+                System.out.printf("B[%d] [%d] =", row, col);
+                B[row][col] = input.nextInt();
             }
         }
         //Printing A Matrix 
         System.out.print("A : ");
         for (int row = 0; row < 2; row++) {
             for (int col = 0; col < 3; col++) {
-                System.out.print(" "+A[row][col]);
+                System.out.print(" " + A[row][col]); 
             }
         }
+        System.out.println(" ");
+        //Printing B Matrix
+
+        System.out.print("B : ");
+        for (int row = 0; row < 2; row++) {
+            for (int col = 0; col < 3; col++) {
+                System.out.print(" " + B[row][col]);
+            }
+        }
+        System.out.println(" ");
     }
 }

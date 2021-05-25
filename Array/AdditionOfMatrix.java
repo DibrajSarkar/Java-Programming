d8996829af61e1ec68da76fb433a694fa7893cbc
DibrajSarkar <hi.dibraj@gmail.com>
2021-05-26 01:03:48 +0600

    Addition Of Matrix in an Array

--- Array/AdditionOfMatrix.java
+++ Array/AdditionOfMatrix.java
@@ -44,5 +44,13 @@ public class AdditionOfMatrix {
                 System.out.print("\t" + B[row][col]);
             }
         }
+       // Addition of A and B Matrix: 
+        System.out.println("\n\n");
+        System.out.println("A+B: ");
+        for (int row = 0; row < 2; row++) {
+            for (int col = 0; col < 3; col++) {
+                System.out.print("\t"+(A[row][col]+B[row][col]));
+            }
+        }
     }
 }

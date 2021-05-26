87189bea13029e21d0d7885885cd3e77ac1324c0
DibrajSarkar <hi.dibraj@gmail.com>
2021-05-26 18:24:55 +0600

    Row Number specific ,but coloum number not specific in an Array

---
+++ Array/ArrayOutputColUnfined.java
@@ -0,0 +1,17 @@
+
+package javaBeginning.Array;
+
+
+public class ArrayOutputColUnfined {
+    public static void main(String[] args) {
+        int [] [] number=new int[4][];
+        number[0]= new int[1];
+        number[1]= new int[2];
+        number[2]= new int[3];
+        number[3]= new int[4];
+        
+        for (int row = 0; row < 4; row++) {
+            
+        }
+    }
+}

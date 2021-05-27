eb25577358a2867318469e29ef12ab3dfd455ad0
DibrajSarkar <hi.dibraj@gmail.com>
2021-05-27 13:55:59 +0600

    Sorting in an Array

--- Array/SortingInArray.java
+++ Array/SortingInArray.java
@@ -6,11 +6,33 @@ import java.util.Arrays;
 
 public class SortingInArray {
     public static void main(String[] args) {
-        int [] number={12,12,24,54,45,56};
+        int [] number={10,-3,18,5,25};
         Arrays.sort(number);
         
+        System.out.println("Ascending:");
         for (int i = 0; i < number.length; i++) {
-            System.out.println(" "+number[i]);
+            System.out.print("\t"+number[i]);
+        }
+        System.out.println(" ");
+        
+        System.out.println("Decending : ");
+        for (int i = number.length -1; i>=0; i--) {
+            System.out.print("\t"+number[i]);
+        }
+        System.out.println(" ");
+        
+        String [] names={"Abdullah", "Rion","Shohan","Mamun"};
+        Arrays.sort(names);// sorting according a to z rules
+      
+        // Ascending wise printing 
+        for (int i = 0; i < names.length; i++) {
+            System.out.print(" "+names[i]);
+        }
+        System.out.println(" ");
+        
+        // Decending wise printing 
+        for (int i = names.length-1; i >=0; i--) {
+            System.out.print(" "+names[i]);
         }
     }
 }

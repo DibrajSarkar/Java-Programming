da8859b33576df5bc8a4b17129447264169ade9c
DibrajSarkar <hi.dibraj@gmail.com>
2021-05-27 13:37:36 +0600

    Sorting in an Array

--- Array/SortingInArray.java
+++ Array/SortingInArray.java
@@ -1,9 +1,16 @@
 
 package javaBeginning.Array;
 
+import java.util.Arrays;
+
 
 public class SortingInArray {
     public static void main(String[] args) {
+        int [] number={12,12,24,54,45,56};
+        Arrays.sort(number);
         
+        for (int i = 0; i < number.length; i++) {
+            System.out.println(" "+number[i]);
+        }
     }
 }

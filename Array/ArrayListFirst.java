65271259c291e522b832783bcc4657c4ee04d4ac
DibrajSarkar <hi.dibraj@gmail.com>
2021-05-28 23:21:57 +0600

    Array listing

--- Array/ArrayListFirst.java
+++ Array/ArrayListFirst.java
@@ -1,9 +1,11 @@
 
 package javaBeginning.Array;
 
+import java.util.Scanner;
 
 public class ArrayListFirst {
     public static void main(String[] args) {
-        
+        Scanner input =new Scanner (System.in);
+        int [] [] numbers =new int[3][4];
     }
 }

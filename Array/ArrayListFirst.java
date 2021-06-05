8be015b58e02745574bb85e1cd6468495422d0a7
DibrajSarkar <hi.dibraj@gmail.com>
2021-06-05 15:14:55 +0600

    Adding elements in ArrayList

--- Array/ArrayListFirst.java
+++ Array/ArrayListFirst.java
@@ -8,5 +8,15 @@ public class ArrayListFirst {
     public static void main(String[] args) {
         ArrayList<Integer> number = new ArrayList<>();
         System.out.println("Size = "+number.size());
+        
+        // Adding elements in Arraylist
+        number.add(10);
+        number.add(11);
+        number.add(12);
+        number.add(13);
+        number.add(4,40);
+        
+        System.out.println(number);
+          
     }
 }

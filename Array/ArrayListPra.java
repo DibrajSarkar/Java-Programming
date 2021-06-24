3acb8530851dbec65c4ebab7c221ab6c480fe055
DibrajSarkar <hi.dibraj@gmail.com>
2021-06-24 13:19:03 +0600

    ArrayList Method

--- Array/ArrayListPra.java
+++ Array/ArrayListPra.java
@@ -21,5 +21,24 @@ public class ArrayListPra {
 //        System.out.println("After Clear ArrayList Contains: "+number);
       boolean is =number1.isEmpty();
         System.out.println("Is it Empty or not: "+is);
+        
+        number2.add(1);
+        number2.add(2);
+        number2.add(3);
+        number2.add(4);
+        
+        System.out.println("ArrayList Contains Number2: "+number2);
+        
+        number3.addAll(number1);
+        
+        System.out.println("Number 3: "+number3);
+        
+      boolean result=  number1.equals(number2);
+      
+        System.out.println("Equals: "+result);
+        
+      boolean result1=  number1.equals(number3);
+      
+        System.out.println("Equals: "+result1);
     }
 }

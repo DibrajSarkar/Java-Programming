035712f315e7f11891aa451d42d2e3827bd3e8cb
SaAbdullah131 <cse.asabdullah@gmail.com>
2024-03-15 05:23:14 +0600

    integer data types bytes in java

---
+++ Core_Java/Variables/Integer.java
@@ -0,0 +1,19 @@
+package Variables;
+
+class Integer {
+    public static void main(String[] args) {
+        // different types of data types in primitive 
+        int num1=9;
+        byte by = 127;
+        short sh = 558;
+        long l = 543438l;
+
+        // float 
+        float f= 5.8f;
+        double d = 5.8;
+
+        char c = 'k';
+
+        boolean b = true;
+    }
+}
--- Core_Java/hello.java
+++ Core_Java/hello.java
@@ -1,7 +1,22 @@
 
 class Hello {
     public  static void main(String[] args) {
+        // variable  in java 
+        int num1 = 30;
+        int num2=45;
+
+        float marks = 6.5f;
+
         System.out.println(3+5);
         System.out.println(11+45);
+
+        System.out.println(num1-num2);
     }
 }
+/**
+ * Primitive Data types are 4 in java
+ * 1.Integer ->byte-1 byte,short->2 bytes,int->4 bytes,long
+ * 2.Float ->double-8 bytes ,float->4 bytes
+ * 3.character -> 2 bytes ( unicode)
+ * 4.Boolean -> true of false (boolean b=true)
+ */

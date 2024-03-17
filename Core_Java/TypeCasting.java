d5ffc05a1bed392556209902464cb27fb62e378e
SaAbdullah131 <cse.asabdullah@gmail.com>
2024-03-18 03:29:45 +0600

    operators in java

--- Core_Java/TypeCasting.java
+++ Core_Java/TypeCasting.java
@@ -3,7 +3,15 @@ public class TypeCasting {
         byte b =127;
         int a = 12;
         byte k = (byte)a;
+ 
+        float f = 5.6f;
+        int t = (int)f;
+        int num=10;
+        int result = ++num;
+        System.out.println(result);
         System.out.println(k);
+        System.out.println(t);
+        System.out.println(num);
 
     }
 }
---
+++ Core_Java/comparsion.java
@@ -0,0 +1,13 @@
+public class comparsion {
+    public static void main(String[] args) {
+        int x = 6;
+        int y = 6;
+        boolean result = x >= y;
+        // > ,< -comparison operator 
+        // ==(double equal to equal or not)
+        // logical operators
+        // AND-&& , OR -|| , NOT ->!
+        
+        System.out.println(result);
+    }
+}
